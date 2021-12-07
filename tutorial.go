3f3a6a241de915ad403da576f3cffaa680126628
unknown <Administrator@jerryslab.local>
2021-12-06 21:18:04 -0500

    first lines of code in golang baby

---
+++ tutorial.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	// variables are defined by "var" followed by the name of the variable, then data structure
+	// data structures: string, int, uint, float64, bool 
+	var name string = "Andrew"
+	// this is what is recomended in golang to define variables ":="
+	name2 := "Tim"
+
+
+}
